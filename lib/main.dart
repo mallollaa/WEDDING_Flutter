a56f211d8216038450c9ea3f263061360267d1dc
Farah <farahaladwanii24@gmail.com>
2023-03-22 12:03:31 +0300

    not  done yet/json

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,8 @@ import 'package:go_router/go_router.dart';
 import 'package:wedding/pages/auth/signin_page.dart';
 import 'package:wedding/pages/auth/signup.dart';
 import 'package:wedding/pages/buttombar/search_page.dart';
+import 'package:wedding/pages/buttombar/vendors/backgroundVendor.dart';
+import 'package:wedding/pages/buttombar/vendors/listV.dart';
 import 'package:wedding/pages/buttombar/vendors/vendors.dart';
 import 'package:wedding/pages/buttombar/vendors/vendors_details_page.dart';
 import 'package:wedding/pages/landing_page.dart';
@@ -53,7 +55,7 @@ class MyApp extends StatelessWidget {
 }
 
 final _router = GoRouter(
-  initialLocation: '/Home',
+  initialLocation: '/categories/listofvendor',
   routes: [
     GoRoute(
       path: '/MyHomePage',
@@ -90,6 +92,14 @@ final _router = GoRouter(
     GoRoute(
       path: '/categories/vendors/detail',
       builder: (context, State) => MyDetailCategoriesPage(),
+    ),
+    GoRoute(
+      path: '/categories/LifOfVendors',
+      builder: (context, State) => BackgroundVendor(),
+    ),
+    GoRoute(
+      path: '/categories/listofvendor',
+      builder: (context, State) => ListVendor(),
     ),
   ],
 );
